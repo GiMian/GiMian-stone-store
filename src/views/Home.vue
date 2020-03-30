21f5a945592dbcbd1170fb79881c69778ed3fd26
GiMian <thgiauth@gmail.com>
2020-03-30 15:12:32 +0700

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-row>
+      <el-col :span="8">
+        <el-button>Default</el-button>
+        <el-button type="primary">Primary</el-button>
+        <el-button type="success">Success</el-button>
+      </el-col>
+      <el-col :span="8">
+        <el-button type="info">Info</el-button>
+        <el-button type="warning">Warning</el-button>
+      </el-col>
+      <el-col :span="8">
+        <el-button type="danger">Danger</el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+<style>
+.el-row {
+  margin-bottom: 20px;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+</style>
